dc4cfc7ccdcae57d4a93160d16195b520b987545
Apels1nov <artemfurm@bk.ru>
2026-03-25 15:45:39 +0300

    ! commit

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "First line" << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!!" << std::endl;
     std::cout << "Photoshop" << std::endl;
     return 0;
 }
